8dbd4e30e1ebcd8d40d7ce800819a2cc1f9ee31b
Ville Oikarinen <ville@oikarinen.org>
2009-09-11 19:46:04 +0000

    hard-coded targets for website, its deploy and tagging
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@8 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-iwant/iwant/only_as_iwant-tool/shell-cli/start_using_iwant_on.sh
+++ iwant-iwant/iwant/only_as_iwant-tool/shell-cli/start_using_iwant_on.sh
@@ -21,6 +21,46 @@ TARGET=$IWANT/as_${WSNAME}-developer
 # TODO incremental
 rm -rf "$TARGET"
 mkdir "$TARGET"
+mkdir "$TARGET/path-to-fresh"
+mkdir "$TARGET/command-to"
+
+function script() {
+    local FILE="$TARGET/$1"
+    cat > "$FILE"
+    chmod u+x "$FILE"
+}
+
+WEBSITE_BUILD="$TARGET/once/website"
+
+script "path-to-fresh/website" <<EOF
+#!/bin/bash
+mkdir -p "$WEBSITE_BUILD"
+cp $IWANT/../../iwant-docs/src/main/html/website/* "$WEBSITE_BUILD/"
+echo "$WEBSITE_BUILD"
+EOF
+
+script "command-to/deploy-website" <<EOF
+echo "Assuming website is uptodate (TODO should be!), type:"
+echo rsync -e ssh --delete-delay -vrucli "$WEBSITE_BUILD/" wipu_@shell.sourceforge.net:iwant-htdocs/
+EOF
+
+script "command-to/tag-deployed-website" <<\EOF
+#!/bin/bash
+if [ $# != 2 ]; then
+  echo "Usage: $0 REV TIME"
+  echo "e.g. $0 555 2009-03-16"
+  exit 1
+fi
+
+REV=$1
+TIME=$2
+
+TAG="${TIME}-website-update"
+SVNBASE=https://iwant.svn.sourceforge.net/svnroot/iwant
+
+echo "Assuming website was updated, type:"
+echo svn cp -r $REV "$SVNBASE/trunk" "$SVNBASE/tags/$TAG" -m \""Tagged $TAG"\"
+EOF
 
 echo "To get access to targets of the $WSNAME project, start your sentences with"
 echo "\$ $TARGET"
