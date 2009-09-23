0be7317cb6a35f99201f23e1e0debca92d1e245b
Ville Oikarinen <ville@oikarinen.org>
2009-09-23 20:18:40 +0000

    online tutorial revision updated
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@26 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-docs/src/main/descript/tutorial/article.sh
+++ iwant-docs/src/main/descript/tutorial/article.sh
@@ -3,7 +3,7 @@ doc 'section {name {Bootstrapping from svn}'
 
 doc 'p {First let'\''s check out <code>iwant</code> from svn
         and bootstrap it as a shell user.}'
-cmd 'svn co -r 16 https://iwant.svn.sourceforge.net/svnroot/iwant/trunk iwant-svn | tail -n 1'
+cmd 'svn co -r 25 https://iwant.svn.sourceforge.net/svnroot/iwant/trunk iwant-svn | tail -n 1'
 cmd 'ls'
 cmd 'cd iwant-svn/iwant-iwant'
 cmd 'iwant/as_shell-user/to_use_iwant.sh'
