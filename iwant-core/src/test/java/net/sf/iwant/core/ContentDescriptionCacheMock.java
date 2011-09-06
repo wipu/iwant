17bf66a4bc7381bf1381246991f32c275a8bc32f
Ville Oikarinen <ville@oikarinen.org>
2011-09-06 07:03:48 +0000

    Fixed one more generics-related warning
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@138 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-core/src/test/java/net/sf/iwant/core/ContentDescriptionCacheMock.java
+++ iwant-core/src/test/java/net/sf/iwant/core/ContentDescriptionCacheMock.java
@@ -10,7 +10,7 @@ public class ContentDescriptionCacheMock implements ContentDescriptionCache {
 	private final Map<Target<?>, String> cache = new HashMap<Target<?>, String>();
 	private final List<Target<?>> recachedTargets = new ArrayList<Target<?>>();
 
-	public void alreadyContains(Target target, String description) {
+	public void alreadyContains(Target<?> target, String description) {
 		cache.put(target, description);
 	}
 
@@ -18,12 +18,12 @@ public class ContentDescriptionCacheMock implements ContentDescriptionCache {
 		return recachedTargets;
 	}
 
-	public void cacheContentDescription(Target target) {
+	public void cacheContentDescription(Target<?> target) {
 		cache.put(target, target.content().definitionDescription());
 		recachedTargets.add(target);
 	}
 
-	public String retrieveContentDescription(Target target) {
+	public String retrieveContentDescription(Target<?> target) {
 		return cache.get(target);
 	}
 
