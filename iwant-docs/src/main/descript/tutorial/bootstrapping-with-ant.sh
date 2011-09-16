4d2b1be5f336e55a07fbacab042be28cb199960c
Ville Oikarinen <ville@oikarinen.org>
2011-09-16 07:32:39 +0000

    empty tutorial for bootstrapping with ant
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@140 b1b70e83-b727-4846-b4cf-edd8a5b4313f

---
+++ iwant-docs/src/main/descript/tutorial/bootstrapping-with-ant.sh
@@ -0,0 +1,14 @@
+end-section() {
+  debuglog "TODO Really, define end-section in descript."
+}
+
+
+doc() {
+
+section 'Boostrapping iwant with ant'
+#------------------------------------
+
+p "TODO."
+end-section
+
+}
