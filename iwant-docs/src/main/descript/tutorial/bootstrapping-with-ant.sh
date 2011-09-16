ae19f5bd30ea11de5302167bd74b6cf25eb92818
Ville Oikarinen <ville@oikarinen.org>
2011-09-16 09:50:19 +0000

    ant bootstrapper generates and reads iwant-from.conf
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@141 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-docs/src/main/descript/tutorial/bootstrapping-with-ant.sh
+++ iwant-docs/src/main/descript/tutorial/bootstrapping-with-ant.sh
@@ -2,13 +2,98 @@ end-section() {
   debuglog "TODO Really, define end-section in descript."
 }
 
+LOCAL_IWANT_ROOT=$(readlink -f "$LOCAL_IWANT/../..")
+
+copy-phase1-xml() {
+cmd 'echo $LOCAL_IWANT'
+cmd "svn export \"$LOCAL_IWANT_ROOT/iwant-bootstrapper/phase1/using-ant\""
+out-was <<EOF
+Export complete.
+EOF
+}
+
+svn-export-phase1-xml() {
+  die "TODO implement"
+}
+
+get-phase1-xml() {
+  debuglog "TODO check LOCAL_IWANT"
+  copy-phase1-xml
+}
+
+antcmd() {
+  cmd 'ant | head -n -1'
+}
+
+phase1-run-1() {
+section "First run to create iwant-from.conf"
+p "The first run creates us a file in which we can specify the iwant revision to use."
+cmd find ..
+out-was <<EOF
+..
+../iw
+../iw/build.xml
+EOF
+failing-cmd 1 ant
+cmd find ..
+out-was <<EOF
+..
+../iw
+../iw/build.xml
+../i-have
+../i-have/iwant-from.conf
+EOF
+cmd 'cat ../i-have/iwant-from.conf'
+end-section
+}
+
+phase1-run-with-incorrect-iwant-from() {
+section "Test handling of incorrect iwant-from.conf"
+p "Bootstrapper complains if iwant-rev is not specified."
+edit "../i-have/iwant-from.conf" empty-file <<EOF
+EOF
+failing-cmd 1 ant
+p "It also complains about missing iwant-url."
+edit "../i-have/iwant-from.conf" only-rev <<EOF
+iwant-rev=
+EOF
+failing-cmd 1 ant
+p "No further side-effects until we fix the issue:"
+cmd find ..
+out-was <<EOF
+..
+../iw
+../iw/build.xml
+../i-have
+../i-have/iwant-from.conf
+EOF
+end-section
+}
+
+phase1-run-with-correct-iwant-from() {
+section "We'll use a local copy of iwant."
+edit "../i-have/iwant-from.conf" use-local-iwant <<EOF
+iwant-rev=
+iwant-url=$LOCAL_IWANT_ROOT
+EOF
+cmd ant
+end-section
+}
 
 doc() {
 
 section 'Boostrapping iwant with ant'
 #------------------------------------
 
-p "TODO."
+cmd 'mkdir -p example/as-example-developer && cd example/as-example-developer'
+get-phase1-xml
+
+cmd 'cd using-ant/iw'
+
+phase1-run-1
+phase1-run-with-incorrect-iwant-from
+phase1-run-with-correct-iwant-from
+
 end-section
 
 }
