032abceb2ce04b7863f0831f2306d688130be024
Ville Oikarinen <ville@oikarinen.org>
2011-10-11 14:35:35 +0000

    targets: iwant main classes, ant bootstrapping tutorial
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@176 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- as-iwant-developer/i-have/wsdef/net/sf/iwant/wsdef/IwantWorkspace.java
+++ as-iwant-developer/i-have/wsdef/net/sf/iwant/wsdef/IwantWorkspace.java
@@ -1,11 +1,15 @@
 package net.sf.iwant.wsdef;
 
+import net.sf.iwant.core.Concatenated;
 import net.sf.iwant.core.Constant;
 import net.sf.iwant.core.ContainerPath;
 import net.sf.iwant.core.EclipseProject;
 import net.sf.iwant.core.EclipseProjects;
+import net.sf.iwant.core.JavaClasses;
 import net.sf.iwant.core.Locations;
 import net.sf.iwant.core.RootPath;
+import net.sf.iwant.core.ScriptGeneratedContent;
+import net.sf.iwant.core.Source;
 import net.sf.iwant.core.Target;
 import net.sf.iwant.core.WorkspaceDefinition;
 
@@ -22,15 +26,46 @@ public class IwantWorkspace implements WorkspaceDefinition {
 			super(locations);
 		}
 
-		public Target<Constant> aConstant() {
-			return target("aConstant").content(
-					Constant.value("Constant generated content\n")).end();
-		}
-
 		public Target<EclipseProjects> eclipseProjects() {
 			return target("eclipse-projects").content(
 					EclipseProjects.with().project(wsdefEclipseProject()))
 					.end();
+		}
+
+		public Target<JavaClasses> iwantCoreMainClasses() {
+			return target("iwantCoreMainClasses").content(
+					JavaClasses.compiledFrom(iwantCoreMainSrc()).using(
+							builtin().ant171classes())).end();
+		}
+
+		private Source iwantCoreMainSrc() {
+			return source("iwant-core/src/main/java");
+		}
+
+		public Target<ScriptGeneratedContent> localAntBootstrappingTutorial() {
+			return target("localAntBootstrappingTutorial").content(
+					ScriptGeneratedContent
+							.of(localAntBootstrappingTutorialScript())).end();
+		}
+
+		public Target<Concatenated> localAntBootstrappingTutorialScript() {
+			// TODO depend on these so we get refreshed when they have been
+			// modified:
+			String descriptSh = locations.wsRoot()
+					+ "/iwant-lib-descript/descript.sh";
+			String docSh = locations.wsRoot()
+					+ "/iwant-docs/src/main/descript/tutorial/bootstrapping-with-ant.sh";
+
+			StringBuilder b = new StringBuilder();
+			b.append("#!/bin/bash\n");
+			b.append("set -eu\n");
+			b.append("DEST=$1\n");
+			b.append("LOCAL_IWANT=\"" + locations.wsRoot()
+					+ "/iwant-iwant/iwant\" bash \"" + descriptSh + "\"");
+			b.append(" \"" + docSh + "\"");
+			b.append(" \"$DEST\" true\n");
+			return target("localAntBootstrappingTutorialScript").content(
+					Concatenated.from().string(b.toString()).end()).end();
 		}
 
 		public EclipseProject wsdefEclipseProject() {
