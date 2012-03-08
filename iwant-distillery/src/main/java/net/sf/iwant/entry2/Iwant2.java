1dae71e1768ce7df9302cd1b3bd09eaf0b6716cc
Ville Oikarinen <ville@oikarinen.org>
2012-03-08 16:44:38 +0000

    entry2 creates example ws-info
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@233 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-distillery/src/main/java/net/sf/iwant/entry2/Iwant2.java
+++ iwant-distillery/src/main/java/net/sf/iwant/entry2/Iwant2.java
@@ -1,5 +1,10 @@
 package net.sf.iwant.entry2;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import net.sf.iwant.entry.Iwant;
 import net.sf.iwant.entry.Iwant.IwantNetwork;
 
 public class Iwant2 {
@@ -17,6 +22,37 @@ public class Iwant2 {
 
 	public static Iwant2 using(IwantNetwork network) {
 		return new Iwant2(network);
+	}
+
+	public static class IwantException extends RuntimeException {
+
+		public IwantException(String message) {
+			super(message);
+		}
+
+	}
+
+	public void evaluate(File asSomeone,
+			@SuppressWarnings("unused") String... args) {
+		File iHave = new File(asSomeone, "i-have");
+		Iwant.ensureDir(iHave);
+		File wsInfo = new File(iHave, "ws-info");
+		createExampleWsInfo(wsInfo);
+		throw new IwantException("I created " + wsInfo
+				+ "\nPlease edit it and rerun me.");
+	}
+
+	private static void createExampleWsInfo(File wsInfo) {
+		try {
+			new FileWriter(wsInfo).append(
+					"# paths are relative to this file's directory\n"
+							+ "WSNAME=example\n" + "WSROOT=../..\n"
+							+ "WSDEF_SRC=wsdef\n"
+							+ "WSDEF_CLASS=com.example.wsdef.Workspace\n")
+					.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
--- iwant-distillery/src/test/java/net/sf/iwant/entry2/Iwant2Test.java
+++ iwant-distillery/src/test/java/net/sf/iwant/entry2/Iwant2Test.java
@@ -1,5 +1,8 @@
 package net.sf.iwant.entry2;
 
+import java.io.File;
+import java.io.IOException;
+
 import junit.framework.TestCase;
 import net.sf.iwant.entry.IwantEntryTestArea;
 import net.sf.iwant.entry.IwantNetworkMock;
@@ -8,16 +11,40 @@ public class Iwant2Test extends TestCase {
 
 	private IwantEntryTestArea testArea;
 	private IwantNetworkMock network;
-	@SuppressWarnings("unused")
 	private Iwant2 iwant2;
+	private File asSomeone;
 
 	public void setUp() {
 		testArea = new IwantEntryTestArea();
 		network = new IwantNetworkMock(testArea);
 		iwant2 = Iwant2.using(network);
+		asSomeone = new File(testArea.root(), "as-test");
 	}
 
-	public void testNothingYet() {
+	private void evaluateAndExpectFriendlyFailureAndExampleWsInfoCreation()
+			throws IOException {
+		try {
+			iwant2.evaluate(asSomeone);
+			fail();
+		} catch (RuntimeException e) {
+			assertEquals("I created " + asSomeone + "/i-have/ws-info\n"
+					+ "Please edit it and rerun me.", e.getMessage());
+		}
+		assertEquals("# paths are relative to this file's directory\n"
+				+ "WSNAME=example\n" + "WSROOT=../..\n" + "WSDEF_SRC=wsdef\n"
+				+ "WSDEF_CLASS=com.example.wsdef.Workspace\n",
+				testArea.contentOf("as-test/i-have/ws-info"));
+	}
+
+	public void testMissingAsSomeoneCausesFriendlyFailureAndExampleCreation()
+			throws IOException {
+		evaluateAndExpectFriendlyFailureAndExampleWsInfoCreation();
+	}
+
+	public void testMissingWsInfoCausesFriendlyFailureAndExampleCreation()
+			throws IOException {
+		testArea.newDir("as-test");
+		evaluateAndExpectFriendlyFailureAndExampleWsInfoCreation();
 	}
 
 }
