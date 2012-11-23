2f4fc7162fd47edd9b9df9f121ab89404e8e51e4
Ville Oikarinen <ville@oikarinen.org>
2012-11-23 10:40:39 +0000

    File regexp removed, not working on wintoys
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@323 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-distillery2/src/main/java/net/sf/iwant/entry3/FileUtil.java
+++ iwant-distillery2/src/main/java/net/sf/iwant/entry3/FileUtil.java
@@ -1,23 +1,43 @@
 package net.sf.iwant.entry3;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * TODO remove need for this class
  */
 public class FileUtil {
 
-	public static String relativePathOfFileUnderParent(File child, File parent)
-			throws IOException {
-		String parentPath = parent.getCanonicalPath();
-		String childPath = child.getCanonicalPath();
-		String replaced = childPath.replaceFirst("^" + parentPath, "");
-		if (replaced.equals(childPath)) {
-			throw new IllegalArgumentException(child + " is not a child of "
-					+ parent);
+	public static String relativePathOfFileUnderParent(File child, File parent) {
+		File absChild = child.getAbsoluteFile();
+		File absParent = parent.getAbsoluteFile();
+		List<String> names = new LinkedList<String>();
+		File parentCandidate = absChild;
+		while (true) {
+			if (parentCandidate == null) {
+				// we are already at root, not a child of parent
+				throw new IllegalArgumentException(absChild
+						+ " is not a child of " + absParent);
+			}
+			if (absParent.equals(parentCandidate)) {
+				// tracking ready
+				break;
+			} else {
+				names.add(0, parentCandidate.getName());
+				parentCandidate = parentCandidate.getParentFile();
+			}
 		}
-		return replaced.replaceFirst("^/", "");
+		StringBuilder b = new StringBuilder();
+		for (Iterator<String> iterator = names.iterator(); iterator.hasNext();) {
+			String name = iterator.next();
+			b.append(name);
+			if (iterator.hasNext()) {
+				b.append("/");
+			}
+		}
+		return b.toString();
 	}
 
 }
--- iwant-distillery2/src/test/java/net/sf/iwant/entry3/FileUtilTest.java
+++ iwant-distillery2/src/test/java/net/sf/iwant/entry3/FileUtilTest.java
@@ -1,13 +1,12 @@
 package net.sf.iwant.entry3;
 
 import java.io.File;
-import java.io.IOException;
 
 import junit.framework.TestCase;
 
 public class FileUtilTest extends TestCase {
 
-	public void testRelativePathFailsWithDifferentParent() throws IOException {
+	public void testRelativePathFailsWithDifferentParent() {
 		File parent = new File("/a");
 		try {
 			FileUtil.relativePathOfFileUnderParent(new File("/b"), parent);
@@ -17,8 +16,7 @@ public class FileUtilTest extends TestCase {
 		}
 	}
 
-	public void testRelativePathFailsWithFileUnderDifferentParent()
-			throws IOException {
+	public void testRelativePathFailsWithFileUnderDifferentParent() {
 		File parent = new File("/a");
 		try {
 			FileUtil.relativePathOfFileUnderParent(new File("/b/c"), parent);
@@ -28,16 +26,48 @@ public class FileUtilTest extends TestCase {
 		}
 	}
 
-	public void testRelativePathIsEmptyForParent() throws IOException {
+	public void testRelativePathFailsWithRoot() {
+		File parent = new File("/a");
+		try {
+			FileUtil.relativePathOfFileUnderParent(new File("/"), parent);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("/ is not a child of /a", e.getMessage());
+		}
+	}
+
+	public void testRelativePathFailsWithNull() {
+		File parent = new File("/a");
+		try {
+			FileUtil.relativePathOfFileUnderParent(null, parent);
+			fail();
+		} catch (NullPointerException e) {
+			// expected
+		}
+	}
+
+	public void testRelativePathIsEmptyForParentItself() {
 		File parent = new File("/a");
 		assertEquals("",
 				FileUtil.relativePathOfFileUnderParent(new File("/a"), parent));
 	}
 
-	public void testRelativePathIsBForAB() throws IOException {
+	public void testRelativePathIsEmptyForRoot() {
+		File parent = new File("/");
+		assertEquals("",
+				FileUtil.relativePathOfFileUnderParent(new File("/"), parent));
+	}
+
+	public void testRelativePathIsBForAB() {
 		File parent = new File("/a");
 		assertEquals("b", FileUtil.relativePathOfFileUnderParent(new File(
 				"/a/b"), parent));
+	}
+
+	public void testRelativePathIsBCForABC() {
+		File parent = new File("/a");
+		assertEquals("b/c", FileUtil.relativePathOfFileUnderParent(new File(
+				"/a/b/c"), parent));
 	}
 
 }
