04bd7c3a44a08a524502e5178fcdfc449e716054
Ville Oikarinen <ville@oikarinen.org>
2012-11-23 09:02:43 +0000

    help.sh should support cygwin
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@320 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-distillery/as-some-developer/with/bash/iwant/help.sh
+++ iwant-distillery/as-some-developer/with/bash/iwant/help.sh
@@ -2,14 +2,25 @@
 
 set -eu
 
+cyg() {
+  local IN=$1
+  case "$(uname)" in
+    CYGWIN*) cygpath --windows -a "$IN" ;;
+    *) echo "$IN" ;;
+  esac
+}
+
 AS_SOMEONE=$(dirname "$0")/../../..
 AS_SOMEONE=$(cd "$AS_SOMEONE" && pwd)
+CYG_AS_SOMEONE=$(cyg "$AS_SOMEONE")
 
 # TODO how to define this only once:
-CACHED=$AS_SOMEONE/.i-cached
+CACHED=$(cyg "$AS_SOMEONE/.i-cached")
 
 CLASSES=$CACHED/.internal/entry-classes
+CYG_CLASSES=$(cyg "$CLASSES")
 mkdir -p "$CLASSES"
-javac -d "$CLASSES" "$AS_SOMEONE/with/java/net/sf/iwant/entry/Iwant.java"
+CYG_SRC=$(cyg "$AS_SOMEONE/with/java/net/sf/iwant/entry/Iwant.java")
+javac -d "$CYG_CLASSES" "$CYG_SRC"
 
-java -cp "$CLASSES" net.sf.iwant.entry.Iwant "$AS_SOMEONE" "$@"
+java -cp "$CYG_CLASSES" net.sf.iwant.entry.Iwant "$CYG_AS_SOMEONE" "$@"
