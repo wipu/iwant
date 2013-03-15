e679f151741f066c057474033af9fefd5e6f7424
Ville Oikarinen <ville@oikarinen.org>
2013-03-15 14:43:52 +0000

    some eclipse settings generated by iwant
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@391 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- as-iwant-developer/i-have/wsdef/src/main/java/net/sf/iwant/wsdef/WorkspaceForIwant.java
+++ as-iwant-developer/i-have/wsdef/src/main/java/net/sf/iwant/wsdef/WorkspaceForIwant.java
@@ -2,10 +2,14 @@ package net.sf.iwant.wsdef;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import net.sf.iwant.api.EclipseSettings;
+import net.sf.iwant.api.FromRepository;
 import net.sf.iwant.api.HelloTarget;
 import net.sf.iwant.api.IwantWorkspace;
+import net.sf.iwant.api.JavaBinModule;
 import net.sf.iwant.api.JavaModule;
 import net.sf.iwant.api.JavaSrcModule;
 import net.sf.iwant.api.JavaSrcModule.IwantSrcModuleSpex;
@@ -23,22 +27,46 @@ public class WorkspaceForIwant implements IwantWorkspace {
 	@Override
 	public List<? extends SideEffect> sideEffects(
 			SideEffectDefinitionContext ctx) {
-		return Arrays.asList(EclipseSettings
-				.with()
-				.name("eclipse-settings")
-				.modules(ctx.wsdefdefJavaModule(), ctx.wsdefJavaModule(),
-						docs()).end());
+		return Arrays.asList(EclipseSettings.with().name("eclipse-settings")
+				.modules(ctx.wsdefdefJavaModule(), ctx.wsdefJavaModule())
+				.modules(allModules()).end());
 	}
 
-	private static IwantSrcModuleSpex iwantModule(String subName) {
+	private static IwantSrcModuleSpex iwantSrcModule(String subName) {
 		String fullName = "iwant-" + subName;
 		return JavaSrcModule.with().name(fullName)
 				.locationUnderWsRoot(fullName).mainJava("src/main/java")
 				.testJava("src/test/java");
 	}
 
+	private static SortedSet<JavaModule> allModules() {
+		// TODO add distillery when >1 mainJava's supported
+		return new TreeSet<JavaModule>(Arrays.asList(/* distillery(), */
+		distillery2(), docs()));
+	}
+
+	private static JavaModule distillery() {
+		return iwantSrcModule("distillery")
+				.mainJava("as-some-developer/with/java")
+				.testDeps(junit(), testarea()).end();
+	}
+
+	private static JavaModule distillery2() {
+		return iwantSrcModule("distillery2").mainDeps(distillery())
+				.testDeps(junit(), testarea()).end();
+	}
+
 	private static JavaModule docs() {
-		return iwantModule("docs").end();
+		return iwantSrcModule("docs").end();
+	}
+
+	private static JavaModule junit() {
+		return JavaBinModule.providing(FromRepository.ibiblio().group("junit")
+				.name("junit").version("4.8.2"));
+	}
+
+	private static JavaModule testarea() {
+		return iwantSrcModule("testarea").end();
 	}
 
 }
