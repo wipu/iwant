66c1b9f023cbab772db202b5166952cfbd00722e
Ville Oikarinen <ville@oikarinen.org>
2013-03-15 14:19:27 +0000

    updated iwant wsdef(def) to compile
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@390 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- as-iwant-developer/i-have/wsdef/src/main/java/net/sf/iwant/wsdef/WorkspaceForIwant.java
+++ as-iwant-developer/i-have/wsdef/src/main/java/net/sf/iwant/wsdef/WorkspaceForIwant.java
@@ -7,36 +7,38 @@ import net.sf.iwant.api.EclipseSettings;
 import net.sf.iwant.api.HelloTarget;
 import net.sf.iwant.api.IwantWorkspace;
 import net.sf.iwant.api.JavaModule;
-import net.sf.iwant.api.JavaModule.JavaModuleSpex;
+import net.sf.iwant.api.JavaSrcModule;
+import net.sf.iwant.api.JavaSrcModule.IwantSrcModuleSpex;
 import net.sf.iwant.api.SideEffect;
 import net.sf.iwant.api.SideEffectDefinitionContext;
 import net.sf.iwant.api.Target;
 
 public class WorkspaceForIwant implements IwantWorkspace {
 
-    @Override
-    public List<? extends Target> targets() {
-        return Arrays.asList(new HelloTarget("hello", "hello from iwant"));
-    }
+	@Override
+	public List<? extends Target> targets() {
+		return Arrays.asList(new HelloTarget("hello", "hello from iwant"));
+	}
 
-    @Override
-    public List<? extends SideEffect> sideEffects(
-            SideEffectDefinitionContext ctx) {
-        return Arrays.asList(EclipseSettings
-                .with()
-                .name("eclipse-settings")
-                .modules(ctx.wsdefdefJavaModule(), ctx.wsdefJavaModule(),
-                        docs()).end());
-    }
+	@Override
+	public List<? extends SideEffect> sideEffects(
+			SideEffectDefinitionContext ctx) {
+		return Arrays.asList(EclipseSettings
+				.with()
+				.name("eclipse-settings")
+				.modules(ctx.wsdefdefJavaModule(), ctx.wsdefJavaModule(),
+						docs()).end());
+	}
 
-    private static JavaModuleSpex iwantModule(String subName) {
-        String fullName = "iwant-" + subName;
-        return JavaModule.with().name(fullName).locationUnderWsRoot(fullName)
-                .mainJava("src/main/java").testJava("src/test/java");
-    }
+	private static IwantSrcModuleSpex iwantModule(String subName) {
+		String fullName = "iwant-" + subName;
+		return JavaSrcModule.with().name(fullName)
+				.locationUnderWsRoot(fullName).mainJava("src/main/java")
+				.testJava("src/test/java");
+	}
 
-    private static JavaModule docs() {
-        return iwantModule("docs").end();
-    }
+	private static JavaModule docs() {
+		return iwantModule("docs").end();
+	}
 
 }
--- as-iwant-developer/i-have/wsdefdef/src/main/java/net/sf/iwant/wsdefdef/WorkspaceProviderForIwant.java
+++ as-iwant-developer/i-have/wsdefdef/src/main/java/net/sf/iwant/wsdefdef/WorkspaceProviderForIwant.java
@@ -2,19 +2,20 @@ package net.sf.iwant.wsdefdef;
 
 import net.sf.iwant.api.IwantWorkspaceProvider;
 import net.sf.iwant.api.JavaModule;
+import net.sf.iwant.api.JavaSrcModule;
 
 public class WorkspaceProviderForIwant implements IwantWorkspaceProvider {
 
-    @Override
-    public JavaModule workspaceModule(JavaModule iwantApiClasses) {
-        return JavaModule.with().name("iwant-workspace")
-                .locationUnderWsRoot("as-iwant-developer/i-have/wsdef")
-                .mainJava("src/main/java").mainDeps(iwantApiClasses).end();
-    }
+	@Override
+	public JavaModule workspaceModule(JavaModule iwantApiClasses) {
+		return JavaSrcModule.with().name("iwant-workspace")
+				.locationUnderWsRoot("as-iwant-developer/i-have/wsdef")
+				.mainJava("src/main/java").mainDeps(iwantApiClasses).end();
+	}
 
-    @Override
-    public String workspaceClassname() {
-        return "net.sf.iwant.wsdef.WorkspaceForIwant";
-    }
+	@Override
+	public String workspaceClassname() {
+		return "net.sf.iwant.wsdef.WorkspaceForIwant";
+	}
 
 }
