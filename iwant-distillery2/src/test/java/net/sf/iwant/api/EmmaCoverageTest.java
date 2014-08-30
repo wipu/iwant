ca4db4695553ad65883ddc70a4ecb10a0fd41099
Ville Oikarinen <ville@oikarinen.org>
2014-08-30 15:43:18 +0000

    1 more test refactored
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@644 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- iwant-distillery2/src/test/java/net/sf/iwant/api/EmmaCoverageTest.java
+++ iwant-distillery2/src/test/java/net/sf/iwant/api/EmmaCoverageTest.java
@@ -1,13 +1,10 @@
 package net.sf.iwant.api;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
-import junit.framework.TestCase;
 import net.sf.iwant.api.javamodules.JavaClasses;
 import net.sf.iwant.api.javamodules.JavaClassesAndSources;
 import net.sf.iwant.api.model.ExternalSource;
@@ -15,64 +12,10 @@ import net.sf.iwant.api.model.HelloTarget;
 import net.sf.iwant.api.model.Path;
 import net.sf.iwant.api.model.Source;
 import net.sf.iwant.api.model.Target;
-import net.sf.iwant.apimocks.CachesMock;
-import net.sf.iwant.apimocks.TargetEvaluationContextMock;
+import net.sf.iwant.apimocks.IwantTestCase;
 import net.sf.iwant.entry.Iwant;
-import net.sf.iwant.entry.Iwant.IwantNetwork;
-import net.sf.iwant.testarea.TestArea;
-import net.sf.iwant.testing.IwantNetworkMock;
 
-public class EmmaCoverageTest extends TestCase {
-
-	private TestArea testArea;
-	private Iwant iwant;
-	private IwantNetwork network;
-	private File tmpDir;
-	private CachesMock caches;
-	private File wsRoot;
-	private TargetEvaluationContextMock ctx;
-	private File cacheDir;
-
-	private PrintStream oldOut;
-
-	private PrintStream oldErr;
-
-	private ByteArrayOutputStream out;
-
-	private ByteArrayOutputStream err;
-
-	@Override
-	public void setUp() {
-		testArea = TestArea.forTest(this);
-		network = new IwantNetworkMock(testArea);
-		iwant = Iwant.using(network);
-		wsRoot = testArea.root();
-		caches = new CachesMock(wsRoot);
-		cacheDir = testArea.newDir("cache");
-		caches.cachesModifiableTargetsAt(cacheDir);
-		tmpDir = new File(testArea.root(), "tmpDir");
-		tmpDir.mkdirs();
-		caches.providesTemporaryDirectoryAt(tmpDir);
-		ctx = new TargetEvaluationContextMock(iwant, caches);
-
-		oldOut = System.out;
-		oldErr = System.err;
-		out = new ByteArrayOutputStream();
-		err = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-		System.setErr(new PrintStream(err));
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-		System.setErr(oldErr);
-		System.setOut(oldOut);
-		System.err.println(err());
-	}
-
-	private String err() {
-		return err.toString();
-	}
+public class EmmaCoverageTest extends IwantTestCase {
 
 	private Path downloaded(Path downloaded) throws IOException {
 		return new ExternalSource(AsEmbeddedIwantUser.with()
