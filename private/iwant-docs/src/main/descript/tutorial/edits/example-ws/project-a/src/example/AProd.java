ff38a77cdabb92570f840b449151492e7ab9b63b
Ville Oikarinen <ville@oikarinen.org>
2014-11-21 20:43:02 +0000

    deleted old tutorial files
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@752 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- private/iwant-docs/src/main/descript/tutorial/edits/example-ws/project-a/src/example/AProd.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-public class AProd {
-
-    public static int value() {
-        return 0;
-    }
-
-}
--- private/iwant-docs/src/main/descript/tutorial/edits/example-ws/project-a/tests/example/ATest.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-public class ATest {
-
-    public static void main(String[] args) {
-        System.out.println("TODO make this a junit test");
-    }
-
-}
