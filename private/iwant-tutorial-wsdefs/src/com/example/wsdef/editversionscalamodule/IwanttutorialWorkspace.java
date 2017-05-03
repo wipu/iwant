271f056c0ce262d690c067ced6972f186eee1cb8
Ville Oikarinen <ville@oikarinen.org>
2017-05-03 16:09:23 +0000

    scalamodule wsdef smaller diff in tutorial
    git-svn-id: https://svn.code.sf.net/p/iwant/code/trunk@887 b1b70e83-b727-4846-b4cf-edd8a5b4313f

--- private/iwant-tutorial-wsdefs/src/com/example/wsdef/editversionscalamodule/IwanttutorialWorkspace.java
+++ private/iwant-tutorial-wsdefs/src/com/example/wsdef/editversionscalamodule/IwanttutorialWorkspace.java
@@ -1,46 +1,80 @@
 package com.example.wsdef.editversionscalamodule;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 import net.sf.iwant.api.core.Concatenated;
+import net.sf.iwant.api.core.Concatenated.ConcatenatedBuilder;
 import net.sf.iwant.api.core.HelloTarget;
+import net.sf.iwant.api.javamodules.JavaBinModule;
 import net.sf.iwant.api.javamodules.JavaSrcModule;
+import net.sf.iwant.api.javamodules.JavaSrcModule.IwantSrcModuleSpex;
 import net.sf.iwant.api.javamodules.ScalaVersion;
+import net.sf.iwant.api.model.Path;
 import net.sf.iwant.api.model.SideEffect;
 import net.sf.iwant.api.model.Target;
 import net.sf.iwant.api.wsdef.SideEffectDefinitionContext;
 import net.sf.iwant.api.wsdef.TargetDefinitionContext;
 import net.sf.iwant.api.wsdef.Workspace;
+import net.sf.iwant.core.javamodules.JavaModules;
 import net.sf.iwant.eclipsesettings.EclipseSettings;
 
 public class IwanttutorialWorkspace implements Workspace {
 
 	private final ScalaVersion scala = ScalaVersion._2_11_7();
-	private final JavaSrcModule mixedScalaAndJava = JavaSrcModule.with()
-			.name("example-mixedscala").scalaVersion(scala)
-			.mainJava("src/main/java").mainScala("src/main/scala").end();
+
+	class ExampleModules extends JavaModules {
+
+		@Override
+		protected IwantSrcModuleSpex commonSettings(IwantSrcModuleSpex m) {
+			return super.commonSettings(m).testDeps(junit);
+		}
+
+		final JavaBinModule hamcrestCore = binModule("org/hamcrest",
+				"hamcrest-core", "1.3");
+		final JavaBinModule junit = binModule("junit", "junit", "4.11",
+				hamcrestCore);
+		final JavaSrcModule helloUtil = srcModule("example-helloutil")
+				.noMainResources().end();
+		final JavaSrcModule hello = srcModule("example-hello").noMainResources()
+				.mainDeps(helloUtil).end();
+		final JavaSrcModule mixedScalaAndJava = JavaSrcModule.with()
+				.name("example-mixedscala").scalaVersion(scala)
+				.mainJava("src/main/java").mainScala("src/main/scala").end();
+
+	}
+
+	private final ExampleModules modules = new ExampleModules();
 
 	@Override
 	public List<? extends Target> targets(TargetDefinitionContext ctx) {
 		return Arrays.asList(new HelloTarget("hello", "hello from iwant\n"),
-				exampleMixedscalaClasspath());
+				classpathStringOfAll(), exampleMixedscalaClasspath());
+	}
+
+	private Target classpathStringOfAll() {
+		ConcatenatedBuilder cp = Concatenated.named("all-as-cp");
+		cp.string(".");
+		for (Path jar : JavaModules
+				.mainArtifactJarsOf(modules.allSrcModules())) {
+			cp.string(File.pathSeparator).nativePathTo(jar);
+		}
+		return cp.end();
 	}
 
 	private Target exampleMixedscalaClasspath() {
 		return Concatenated.named("example-mixedscala-classpath")
-				.unixPathTo(mixedScalaAndJava.mainArtifact()).string(":")
-				.unixPathTo(scala.libraryJar()).end();
+				.unixPathTo(modules.mixedScalaAndJava.mainArtifact())
+				.string(":").unixPathTo(scala.libraryJar()).end();
 	}
 
 	@Override
 	public List<? extends SideEffect> sideEffects(
 			SideEffectDefinitionContext ctx) {
-		return Arrays
-				.asList(EclipseSettings.with().name("eclipse-settings")
-						.modules(ctx.wsdefdefJavaModule(),
-								ctx.wsdefJavaModule(), mixedScalaAndJava)
-						.end());
+		return Arrays.asList(EclipseSettings.with().name("eclipse-settings")
+				.modules(ctx.wsdefdefJavaModule(), ctx.wsdefJavaModule())
+				.modules(modules.allSrcModules()).end());
 	}
 
 }
