22491c7d2d37e258ea27a31026f9daf8e1fdbc9c
Ville Oikarinen <ville@oikarinen.org>
2017-10-19 17:58:41 +0300

    removed (unnecessary) sf.net refs from website

--- private/iwant-docs/src/main/descript/tutorial/iwantmore.sh
+++ private/iwant-docs/src/main/descript/tutorial/iwantmore.sh
@@ -2,13 +2,13 @@ doc-content() {
 
 p "This is the end of the tutorial. Thank you for reading."
 
-p "Only a small subset of features is currently covered here. When you have a functional workspace, try finding all subclasses of Target (ctrl-T over Target in Eclipse, for example) to see what is available. You may learn to use them by just using code completion. You can also checkout the whole iwant workspace from sourceforge and read its own build and tests."
+p "Only a small subset of features is currently covered here. When you have a functional workspace, try finding all subclasses of Target (ctrl-T over Target in Eclipse, for example) to see what is available. You may learn to use them by just using code completion. You can also clone the whole iwant workspace from github and read its own build and tests."
 
 p "But now, after years of developing iwant only for myself and for people working with me, I need feedback to prioritize my work. What do you want next? What needs to be added to the documentation? Or the implementation? What needs to be fixed? I use almost exclusively Linux, so feedback from Windows and Mac users is very much appreciated. (Especially Mac, since I haven't tried iwant on it.)"
 
 p "Or even better: do you want to share your Target (or SideEffect) implementations, to be added as plugins to iwant?"
 
-p "You can contact me either using sourceforge services or email: <myfirstname>@<mylastname>.org."
+p "You can contact me by email: <myfirstname>@<mylastname>.org."
 
 p "- Ville Oikarinen"
 
