2a48cf453d6b83812ba1b304b6db4faaabe093d5
Ville Oikarinen <ville@oikarinen.org>
2017-10-19 17:23:10 +0300

    website and its refresh script

---
+++ as-iwant-developer/iwantish/docs-refreshed.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+set -o xtrace
+
+HERE=$(dirname "$0")
+AS_ME=$HERE/..
+WSROOT=$AS_ME/..
+WEBSITE=$("$AS_ME/with/bash/iwant/help.sh" target/remote-website/as-path)
+
+DOCS=$WSROOT/docs
+
+rm -rf "$DOCS"
+mkdir "$DOCS"
+
+cp -av "$WEBSITE"/* "$DOCS"/
+echo -n iwant-build.org > "$DOCS/CNAME"
