1bf47498276e87ae1fad7699a3b4e0447232d78b
Ville Oikarinen <ville@oikarinen.org>
2023-05-12 18:20:35 +0300

    Optimized TargetNameChecker

--- essential/iwant-entry3/src/main/java/org/fluentjava/iwant/entry3/TargetNameChecker.java
+++ essential/iwant-entry3/src/main/java/org/fluentjava/iwant/entry3/TargetNameChecker.java
@@ -1,6 +1,8 @@
 package org.fluentjava.iwant.entry3;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.fluentjava.iwant.api.model.Path;
 import org.fluentjava.iwant.api.model.Target;
@@ -8,15 +10,20 @@ import org.fluentjava.iwant.api.model.Target;
 class TargetNameChecker {
 
 	public static void check(List<? extends Target> targets) {
+		Set<String> seen = new HashSet<>();
 		for (Target target : targets) {
-			check(target);
+			check(target, seen);
 		}
 	}
 
-	private static void check(Path path) {
+	private static void check(Path path, Set<String> seen) {
+		if (seen.contains(path.name())) {
+			return;
+		}
 		check(path.name());
+		seen.add(path.name());
 		for (Path ingredient : path.ingredients()) {
-			check(ingredient);
+			check(ingredient, seen);
 		}
 	}
 
---
+++ essential/iwant-entry3/src/test/java/org/fluentjava/iwant/entry3/TargetNameCheckerMicrobenchmarkTest.java
@@ -0,0 +1,54 @@
+package org.fluentjava.iwant.entry3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.fluentjava.iwant.api.core.Concatenated;
+import org.fluentjava.iwant.api.core.Concatenated.ConcatenatedBuilder;
+import org.fluentjava.iwant.api.model.Target;
+import org.junit.Test;
+
+public class TargetNameCheckerMicrobenchmarkTest {
+
+	private static final int LEVEL_COUNT = 10;
+	private static final int TARGETS_PER_LEVEL = 100;
+
+	@Test
+	public void checkABigTree() {
+		System.err.println("Creating target tree");
+		List<Target> targets = new ArrayList<>();
+		Map<String, Target> targetCache = new HashMap<>();
+		for (int i = 0; i < TARGETS_PER_LEVEL; i++) {
+			Target t = target(targetCache, 0, i);
+			targets.add(t);
+		}
+		System.err
+				.println("Done creating target tree, checking target names...");
+		long t1 = System.currentTimeMillis();
+		TargetNameChecker.check(targets);
+		long t2 = System.currentTimeMillis();
+		System.err.println("Checking target names took " + (t2 - t1) + "ms.");
+	}
+
+	private static Target target(Map<String, Target> targetCache, int level,
+			int index) {
+		String name = "t-" + level + "-" + index;
+		Target cached = targetCache.get(name);
+		if (cached != null) {
+			return cached;
+		}
+		ConcatenatedBuilder t = Concatenated.named(name);
+		if (level < LEVEL_COUNT) {
+			for (int i = 0; i < TARGETS_PER_LEVEL; i++) {
+				Target dep = target(targetCache, level + 1, i);
+				t.unixPathTo(dep);
+			}
+		}
+		Concatenated target = t.end();
+		targetCache.put(name, target);
+		return target;
+	}
+
+}
